2f85e0e81cb7616f135ac6fc0175f107352b615f
Sergiy Leshchenko <sergrut99@gmail.com>
2022-07-27 12:41:41 +0300

    Add project files.

---
+++ BulkyBookWeb/Helpers/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using BulkyBookWeb.Data;
+global using Microsoft.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations;
---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
